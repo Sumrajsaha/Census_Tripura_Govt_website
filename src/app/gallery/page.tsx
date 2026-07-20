08294b818431e66adb7fa3bcff3ab59446640134
Sumraj Saha <sumrajsaha765@gmail.com>
2026-07-20 11:36:08 +0530

    fix update

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -20,7 +20,9 @@ export default function GalleryPage() {
 
   const galleryImages = [
     { src: '/G-1.jpeg', altEn: '.', altHi: 'जनगणना फील्ड प्रशिक्षण गतिविधियाँ' },
+    { src: '/G-2.jpeg', altEn: '.', altHi: '.' },
     { src: '/G-3.jpeg', altEn: '.', altHi: 'जनगणना कार्य निदेशालय त्रिपुरा क्षेत्रीय सम्मेलन' },
+    { src: '/G-4.jpeg', altEn: '.', altHi: 'जनगणना फील्ड प्रशिक्षण गतिविधियाँ' },
   ];
 
   const galleryVideos = [
