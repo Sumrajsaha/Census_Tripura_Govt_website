2d76232210656452efc99c6eb867ec56832b1cc8
Sumraj Saha <sumrajsaha765@gmail.com>
2026-07-20 21:15:36 +0530

    fix update

--- src/app/about/directory/page.tsx
+++ src/app/about/directory/page.tsx
@@ -126,7 +126,7 @@ export default function DirectoryPage() {
       email: '.'
     },
     {
-      name: 'Shri Arupla Harshavardhan',
+      name: 'Shri Rahul Das',
       designationEn: 'Statistical Investigator Grade-II',
       designationHi: 'सांख्यिकीय अन्वेषक ग्रेड -II',
       landline: '0381-2336442',
@@ -140,7 +140,7 @@ export default function DirectoryPage() {
       email: '.'
     },
     {
-      name: 'Shri Rahul Das',
+      name: 'Shri Arupla Harshavardhan',
       designationEn: 'Statistical Investigator Grade-II',
       designationHi: 'सांख्यिकीय अन्वेषक ग्रेड -II',
       landline: '0381-2336442',
